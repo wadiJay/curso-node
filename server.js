ce85cbb000fa7bd8a9f65e0be292b46e4638cd00
Wadi <marianojacobo@gmail.com>
2018-11-02 13:00:55 -0300

    Setup start script

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials/');
@@ -47,4 +48,4 @@ app.get('/about', (req, res) => {
     });
 })
 
-app.listen(3000);
+app.listen(port);
