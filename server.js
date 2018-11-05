787f785d56faa1d7be49bbddeb2892de3fd47489
Wadi <marianojacobo@gmail.com>
2018-11-05 12:12:46 -0300

    New features!

--- server.js
+++ server.js
@@ -48,4 +48,10 @@ app.get('/about', (req, res) => {
     });
 })
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page'
+    });
+})
+
 app.listen(port);
